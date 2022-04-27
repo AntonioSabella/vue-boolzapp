778745241b885408fc712b46c2f44ef4a1ad300e
AntonioSabella <asabella6@gmail.com>
2022-04-27 17:31:25 +0200

    Add milestone 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,17 @@
 
 /* Descrizione:
 Replicare famoso sito messaggistica instantanea.
-Milestone 1
+Milestone 1(done)
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
-Milestone 2
+Milestone 2(done)
 Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 Click sul contatto mostra la conversazione del contatto cliccato
+Milestone 3(semi-done -- non funziona il setTimeout)
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+“enter” il testo viene aggiunto al thread sopra, come messaggio verde.
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+un “ok” come risposta, che apparirà dopo 1 secondo.
 */
 
 const app = new Vue ({
@@ -177,8 +182,29 @@ const app = new Vue ({
                 ]
             }
         ],
+        userNewMessage: {
+            date: new Date().toLocaleString([], {hour: '2-digit', minute:'2-digit'}),
+            message: '',
+            status: 'sent'
+        },
+        friendAnswer : {
+            date: new Date().toLocaleString([], {hour: '2-digit', minute:'2-digit'}),
+            message: "OK",
+            status: "received"
+        },
     },
     methods : {
-        
+        myNewMessage() {
+            //console.log('messaggio inviato');
+            //console.log(this.contacts[this.active_friend].messages);
+            this.contacts[this.active_friend].messages.push(this.userNewMessage);
+            this.contacts[this.active_friend].messages.push(this.friendAnswer);
+            // ricontrollare selezione con il setTimeout() -- perché non funziona la selezione se inserisco la timing function?
+            this.userNewMessage = {
+                date: new Date().toLocaleString([], {hour: '2-digit', minute:'2-digit'}),
+                message: '',
+                status: 'sent',
+            };
+        },
     },
 })
